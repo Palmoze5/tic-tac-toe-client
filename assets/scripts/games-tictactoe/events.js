610c71ca3cf3aed4a7427f7216c5413f5bfa18a0
Palmoze5 <ascottpalmoze@gmail.com>
2019-01-08 20:27:09 -0500

    Events committed for another merging

--- assets/scripts/games-tictactoe/events.js
+++ assets/scripts/games-tictactoe/events.js
@@ -4,7 +4,7 @@
 // const ui = require('./ui.js')
 // const store = require('./ui.js')
 
-// let player = 'x'
+// const playerOne = 'x'
 // const gameBoard = ['', '', '', '', '', '', '', '', '']
 let turn = 1
 
@@ -14,31 +14,67 @@ const onBoxClick = function (event) {
   event.preventDefault()
   // $('#box')
   // we want this function to display either "X" or "O"
-  const idOfBoxClicked = event.target.id
-  // console.log('event.Target is ', event.target)
-  // console.log('id is ', idOfBoxClicked)
-  // const boxSelector = '#' + idOfBoxClicked
-  // console.log('boxSelector is ', boxSelector)
+}
+const idOfBoxClicked = event.target.id
+// console.log('event.Target is ', event.target)
+// console.log('id is ', idOfBoxClicked)
+// const boxSelector = '#' + idOfBoxClicked
+// console.log('boxSelector is ', boxSelector)
 
-  const boxText = $('#' + idOfBoxClicked).text()
-  // console.log('boxText is ', boxText)
+const boxText = $('#' + idOfBoxClicked).text()
+// console.log('boxText is ', boxText)
 
-  if (boxText === '') {
-    if (turn % 2 === 1) {
-      $('#' + idOfBoxClicked).text('x')
-      gameBoard[idOfBoxClicked] = 'x'
-      turn++
-    } else if (turn % 2 === 0) {
-      $('#' + idOfBoxClicked).text('o')
-      gameBoard[idOfBoxClicked] = 'o'
-      turn++
-    }
-    console.log(gameBoard)
+if (boxText === '') {
+  if (turn % 2 === 1) {
+    $('#' + idOfBoxClicked).text('x')
+    gameBoard[idOfBoxClicked] = 'x'
+    turn++
+  } else if (turn % 2 === 0) {
+    $('#' + idOfBoxClicked).text('o')
+    gameBoard[idOfBoxClicked] = 'o'
+    turn++
   }
-
+}
+//   const gameWinner = function gameWinner() {
+//     if (store.gameBoard.cells[0] === store.gameBoard.cells[1] && store.gameBoard.cells[0] === store.gameBoard.cells[2] && store.gameBoard.cells[0] !== '') {
+//       return true;
+//     } else if (store.gameBoard.cells[3] === store.gameBoard.cells[4] && store.gameBoard.cells[3] === store.gameBoard.cells[5] && store.gameBoard.cells[3] !== '') {
+//       return true;
+//     } else if (store.gameBoard.cells[6] === store.gameBoard.cells[7] && store.gameBoard.cells[6] === store.gameBoard.cells[8] && store.gameBoard.cells[6] !== '') {
+//       return true;
+//     } else if (store.gameBoard.cells[0] === store.gameBoard.cells[3] && store.gameBoard.cells[0] === store.gameBoard.cells[6] && store.gameBoard.cells[0] !== '') {
+//     return true;
+//   } else if (store.gameBoard.cells[1] === store.gameBoard.cells[4] && store.gameBoard.cells[1] === store.gameBoard.cells[7] && store.gameBoard.cells[1] !== '') {
+//     return true;
+//   } else if (store.gameBoard.cells[2] === store.gameBoard.cells[5] && store.gameBoard.cells[2] === store.gameBoard.cells[8] && store.gameBoard.cells[2] !== '') {
+//     return true;
+//   } else if (store.gameBoard.cells[0] === store.gameBoard.cells[4] && store.gameBoard.cells[0] === store.gameBoard.cells[8] && store.gameBoard.cells[0] !== '') {
+//     return true;
+//   } else if (store.gameBoard.cells[2] === store.gameBoard.cells[4] && store.gameBoard.cells[2] === store.gameBoard.cells[6] && store.gameBoard.cells[2] !== '') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+//   }
+// };
+// // check for draw scenario
+// var gameDraw = function gameDraw() {
+//   if (store.gameBoard.cells[0] && store.gameBoard.cells[1] && store.gameBoard.cells[2] && store.gameBoard.cells[3] && store.gameBoard.cells[4] && store.gameBoard.cells[5] && store.gameBoard.cells[6] && store.gameBoard.cells[7] && store.gameBoard.cells[8] !== '' && checkWinner() === false) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// // check for if game is over
+// var gameOver = function gameOver() {
+//   if (checkWinner() === true || gameDraw() === true) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 // Telling the user that the game is over:
 // 1.) When the gameboard fills up
-}
 const addGameHandlers = function () {
   $('.box').on('click', onBoxClick)
   // event.preventDefault()
@@ -46,5 +82,8 @@ const addGameHandlers = function () {
 }
 module.exports = {
   addGameHandlers
+  // gameWinner,
+  // gameDraw,
+  // gameOver
   // createNewGame
 }
