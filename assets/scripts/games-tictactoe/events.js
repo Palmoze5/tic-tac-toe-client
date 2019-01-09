df551fbfc5b0cbdb6701519e98e8301ac0b01bb5
Palmoze5 <ascottpalmoze@gmail.com>
2019-01-09 16:42:23 -0500

    quick commit  -Events JS

--- assets/scripts/games-tictactoe/events.js
+++ assets/scripts/games-tictactoe/events.js
@@ -8,73 +8,6 @@ const store = require('./ui.js')
 // const gameBoard = ['', '', '', '', '', '', '', '', '']
 let turn = 1
 
-<<<<<<< HEAD
-const gameBoard = ['', '', '', '', '', '', '', '', '']
-const winningConditions = [
-  ['0', '1', '2'],
-  ['3', '4', '5'],
-  ['6', '7', '8'],
-  ['0', '3', '6'],
-  ['2', '4', '7'],
-  ['3', '5', '8'],
-  ['0', '4', '8'],
-  ['2', '4', '6']
-]
-
-const onBoxClick = function (event) {
-  event.preventDefault()
-  const idOfBoxClicked = event.target.id
-  // console.log(event)
-  // $('#box')
-  // we want this function to display either "X" or "O"
-  // const idOfBoxClicked = event.target.id
-  // console.log('event.Target is ', event.target)
-  // console.log('id is ', idOfBoxClicked)
-  // const boxSelector = '#' + idOfBoxClicked
-  // console.log('boxSelector is ', boxSelector)
-
-  const boxText = $('#' + idOfBoxClicked).text()
-  // console.log('boxText is ', boxText)
-
-  if (boxText === '') {
-    if (turn % 2 === 1) {
-      $('#' + idOfBoxClicked).text('x')
-      gameBoard[idOfBoxClicked] = 'x'
-      turn++
-    } else if (turn % 2 === 0) {
-      $('#' + idOfBoxClicked).text('o')
-      gameBoard[idOfBoxClicked] = 'o'
-      turn++
-    }
-  }
-  for (let i = 0; i < gameBoard.length; i++) {
-
-    // console.log(gameBoard)
-    // console.log(winningConditions)
-  }
-}
-//   const gameWinner = function gameWinner() {
-//     if (store.gameBoard.cells[0] === store.gameBoard.cells[1] && store.gameBoard.cells[0] === store.gameBoard.cells[2] && store.gameBoard.cells[0] !== '') {
-//       return true;
-//     } else if (store.gameBoard.cells[3] === store.gameBoard.cells[4] && store.gameBoard.cells[3] === store.gameBoard.cells[5] && store.gameBoard.cells[3] !== '') {
-//       return true;
-//     } else if (store.gameBoard.cells[6] === store.gameBoard.cells[7] && store.gameBoard.cells[6] === store.gameBoard.cells[8] && store.gameBoard.cells[6] !== '') {
-//       return true;
-//     } else if (store.gameBoard.cells[0] === store.gameBoard.cells[3] && store.gameBoard.cells[0] === store.gameBoard.cells[6] && store.gameBoard.cells[0] !== '') {
-//     return true;
-//   } else if (store.gameBoard.cells[1] === store.gameBoard.cells[4] && store.gameBoard.cells[1] === store.gameBoard.cells[7] && store.gameBoard.cells[1] !== '') {
-//     return true;
-//   } else if (store.gameBoard.cells[2] === store.gameBoard.cells[5] && store.gameBoard.cells[2] === store.gameBoard.cells[8] && store.gameBoard.cells[2] !== '') {
-//     return true;
-//   } else if (store.gameBoard.cells[0] === store.gameBoard.cells[4] && store.gameBoard.cells[0] === store.gameBoard.cells[8] && store.gameBoard.cells[0] !== '') {
-//     return true;
-//   } else if (store.gameBoard.cells[2] === store.gameBoard.cells[4] && store.gameBoard.cells[2] === store.gameBoard.cells[6] && store.gameBoard.cells[2] !== '') {
-//     return true;
-//   } else {
-//     return false;
-//   }
-//   }
-=======
 store.gameBoard = ['', '', '', '', '', '', '', '', '']
 
 const winningGame = function () {
@@ -147,7 +80,6 @@ const onBoxClick = function (event) {
 // console.log(gameBoard)
 // console.log(winningConditions)
 // }
->>>>>>> game
 // };
 // // check for draw scenario
 // var gameDraw = function gameDraw() {
